18396fbed6f0e6edfeb2ee991a206a2793bffdbf
jasonchenonline <jxc_2003sg@yahoo.com>
2017-03-14 23:33:31 +0800

    Concurrency with tester.

--- QWenda/src/main/java/com/nowcoder/controller/QuestionController.java
+++ QWenda/src/main/java/com/nowcoder/controller/QuestionController.java
@@ -34,64 +34,12 @@ public class QuestionController {
     @Autowired
     UserService userService;
 
-    //@Autowired
-    //CommentService commentService;
-
-    //@Autowired
-    //FollowService followService;
-
-    //@Autowired
-    //LikeService likeService;
-
-    //@Autowired
-    //EventProducer eventProducer;
 
     @RequestMapping(value = "/question/{qid}", method = {RequestMethod.GET})
     public String questionDetail(Model model, @PathVariable("qid") int qid) {
         Question question = questionService.getById(qid);
         model.addAttribute("question", question);
-
-/*
-        List<Comment> commentList = commentService.getCommentsByEntity(qid, EntityType.ENTITY_QUESTION);
-        List<ViewObject> comments = new ArrayList<ViewObject>();
-        for (Comment comment : commentList) {
-            ViewObject vo = new ViewObject();
-            vo.set("comment", comment);
-            if (hostHolder.getUser() == null) {
-                vo.set("liked", 0);
-            } else {
-                vo.set("liked", likeService.getLikeStatus(hostHolder.getUser().getId(), EntityType.ENTITY_COMMENT, comment.getId()));
-            }
-
-            vo.set("likeCount", likeService.getLikeCount(EntityType.ENTITY_COMMENT, comment.getId()));
-            vo.set("user", userService.getUser(comment.getUserId()));
-            comments.add(vo);
-        }
-
-        model.addAttribute("comments", comments);
-
-        List<ViewObject> followUsers = new ArrayList<ViewObject>();
-        // 获取关注的用户信息
-        List<Integer> users = followService.getFollowers(EntityType.ENTITY_QUESTION, qid, 20);
-        for (Integer userId : users) {
-            ViewObject vo = new ViewObject();
-            User u = userService.getUser(userId);
-            if (u == null) {
-                continue;
-            }
-            vo.set("name", u.getName());
-            vo.set("headUrl", u.getHeadUrl());
-            vo.set("id", u.getId());
-            followUsers.add(vo);
-        }
-        model.addAttribute("followUsers", followUsers);
-        if (hostHolder.getUser() != null) {
-            model.addAttribute("followed", followService.isFollower(hostHolder.getUser().getId(), EntityType.ENTITY_QUESTION, qid));
-        } else {
-            model.addAttribute("followed", false);
-        }
-*/
-
+        model.addAttribute("user", userService.getUser(question.getUserId()));
         return "detail";
     }
 
@@ -110,15 +58,12 @@ public class QuestionController {
                 question.setUserId(hostHolder.getUser().getId());
             }
             if (questionService.addQuestion(question) > 0) {
-//                eventProducer.fireEvent(new EventModel(EventType.ADD_QUESTION)
-//                        .setActorId(question.getUserId()).setEntityId(question.getId())
-//                        .setExt("title", question.getTitle()).setExt("content", question.getContent()));
                 return WendaUtil.getJSONString(0);
             }
         } catch (Exception e) {
-            logger.error("增加题目失败" + e.getMessage());
+            logger.error("Failed to add question" + e.getMessage());
         }
-        return WendaUtil.getJSONString(1, "失败");
+        return WendaUtil.getJSONString(1, "Failed");
     }
 
 }
