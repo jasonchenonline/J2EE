b610a082deecab82a41c1031d7a18e7dcbff5e7f
jasonchenonline <jxc_2003sg@yahoo.com>
2017-04-09 22:32:34 +0800

    SNS Follow/unfollow service

---
+++ QWenda/src/main/java/com/nowcoder/service/FollowService.java
@@ -0,0 +1,114 @@
+package com.nowcoder.service;
+
+import com.nowcoder.util.JedisAdapter;
+import com.nowcoder.util.RedisKeyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by JasonC on 4/9/2017.
+ */
+@Service
+public class FollowService {
+    @Autowired
+    JedisAdapter jedisAdapter;
+
+    /**
+     * It's common service. Can follow any entity, be a question, user, comments or anything
+     * @param userId
+     * @param entityType
+     * @param entityId
+     * @return
+     */
+    public boolean follow(int userId, int entityType, int entityId) {
+        String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        Date date = new Date();
+        //Follower add me. Need to be transact with next step
+        Jedis jedis = jedisAdapter.getJedis();
+        Transaction tx = jedisAdapter.multi(jedis);
+        tx.zadd(followerKey, date.getTime(), String.valueOf(userId));
+        //myself add as followee
+        tx.zadd(followeeKey, date.getTime(), String.valueOf(entityId));
+        List<Object> ret = jedisAdapter.exec(tx, jedis);
+        return ret.size() == 2 && (Long) ret.get(0) > 0 && (Long) ret.get(1) > 0;
+    }
+
+    /**
+     * unfollow
+     * @param userId
+     * @param entityType
+     * @param entityId
+     * @return
+     */
+    public boolean unfollow(int userId, int entityType, int entityId) {
+        String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        Date date = new Date();
+        Jedis jedis = jedisAdapter.getJedis();
+        Transaction tx = jedisAdapter.multi(jedis);
+        // Follower remove me
+        tx.zrem(followerKey, String.valueOf(userId));
+        // myself remove followee
+        tx.zrem(followeeKey, String.valueOf(entityId));
+        List<Object> ret = jedisAdapter.exec(tx, jedis);
+        return ret.size() == 2 && (Long) ret.get(0) > 0 && (Long) ret.get(1) > 0;
+    }
+
+    public List<Integer> getFollowers(int entityType, int entityId, int count) {
+        String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+        return getIdsFromSet(jedisAdapter.zrevrange(followerKey, 0, count));
+    }
+
+    public List<Integer> getFollowers(int entityType, int entityId, int offset, int count) {
+        String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+        return getIdsFromSet(jedisAdapter.zrevrange(followerKey, offset, offset+count));
+    }
+
+    public List<Integer> getFollowees(int userId, int entityType, int count) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        return getIdsFromSet(jedisAdapter.zrevrange(followeeKey, 0, count));
+    }
+
+    public List<Integer> getFollowees(int userId, int entityType, int offset, int count) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        return getIdsFromSet(jedisAdapter.zrevrange(followeeKey, offset, offset+count));
+    }
+
+    public long getFollowerCount(int entityType, int entityId) {
+        String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+        return jedisAdapter.zcard(followerKey);
+    }
+
+    public long getFolloweeCount(int userId, int entityType) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        return jedisAdapter.zcard(followeeKey);
+    }
+
+    private List<Integer> getIdsFromSet(Set<String> idset) {
+        List<Integer> ids = new ArrayList<>();
+        for (String str : idset) {
+            ids.add(Integer.parseInt(str));
+        }
+        return ids;
+    }
+
+    /**
+     *  To check if this user is follower
+     * @param userId
+     * @param entityType
+     * @param entityId
+     * @return
+     */
+    public boolean isFollower(int userId, int entityType, int entityId) {
+        String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+        return jedisAdapter.zscore(followerKey, String.valueOf(userId)) != null;
+    }
+}
