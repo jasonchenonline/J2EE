d8ed659fa4806f03e2e2c15fd6e7845d4e379e7a
jasonchenonline <jxc_2003sg@yahoo.com>
2017-04-19 07:29:03 +0800

    news feed service.

---
+++ QWenda/src/main/java/com/nowcoder/async/handler/FeedHandler.java
@@ -0,0 +1,96 @@
+package com.nowcoder.async.handler;
+
+import com.alibaba.fastjson.JSONObject;
+import com.nowcoder.async.EventHandler;
+import com.nowcoder.async.EventModel;
+import com.nowcoder.async.EventType;
+import com.nowcoder.model.*;
+import com.nowcoder.service.*;
+import com.nowcoder.util.JedisAdapter;
+import com.nowcoder.util.RedisKeyUtil;
+import com.nowcoder.util.WendaUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+/**
+ * Created by JasonC on 4/9/2017.
+ */
+@Component
+public class FeedHandler implements EventHandler {
+    @Autowired
+    FollowService followService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    FeedService feedService;
+
+    @Autowired
+    JedisAdapter jedisAdapter;
+
+    @Autowired
+    QuestionService questionService;
+
+
+    private String buildFeedData(EventModel model) {
+        Map<String, String> map = new HashMap<String ,String>();
+        // standard method to get user
+        User actor = userService.getUser(model.getActorId());
+        if (actor == null) {
+            return null;
+        }
+        map.put("userId", String.valueOf(actor.getId()));
+        map.put("userHead", actor.getHeadUrl());
+        map.put("userName", actor.getName());
+
+        if (model.getType() == EventType.COMMENT ||
+                (model.getType() == EventType.FOLLOW  && model.getEntityType() == EntityType.ENTITY_QUESTION)) {
+            Question question = questionService.getById(model.getEntityId());
+            if (question == null) {
+                return null;
+            }
+            map.put("questionId", String.valueOf(question.getId()));
+            map.put("questionTitle", question.getTitle());
+            return JSONObject.toJSONString(map);
+        }
+        return null;
+    }
+
+    @Override
+    public void doHandle(EventModel model) {
+        // Test Only, get a Random userID
+        Random r = new Random();
+        model.setActorId(1+r.nextInt(10));
+
+        // create a Feed
+        Feed feed = new Feed();
+        feed.setCreatedDate(new Date());
+        feed.setType(model.getType().getValue());
+        feed.setUserId(model.getActorId());
+        feed.setData(buildFeedData(model));
+        if (feed.getData() == null) {
+            // invalid Feed
+            return;
+        }
+        feedService.addFeed(feed);
+
+        // get all followers
+        List<Integer> followers = followService.getFollowers(EntityType.ENTITY_USER, model.getActorId(), Integer.MAX_VALUE);
+        // 系统队列
+        followers.add(0);
+        // Push feed to all followers
+        for (int follower : followers) {
+            String timelineKey = RedisKeyUtil.getTimelineKey(follower);
+            jedisAdapter.lpush(timelineKey, String.valueOf(feed.getId()));
+            //To do. Limit size. Remove all other feed if the length of timelineKey is too long.
+        }
+    }
+
+    @Override
+    public List<EventType> getSupportEventTypes() {
+        return Arrays.asList(new EventType[]{EventType.COMMENT, EventType.FOLLOW});
+    }
+}
